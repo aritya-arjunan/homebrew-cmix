2d61f7be3abc5bc91a914fb0246b12c324d84415
Aritya Vitendra Arjunan <aritya.wps@gmail.com>
2026-01-13 15:43:32 +1100

    correct linux

--- Formula/cmix.rb
+++ Formula/cmix.rb
@@ -6,23 +6,36 @@ class Cmix < Formula
   license "GPL-3.0-or-later"
 
   def install
-    sources = Dir.glob("src/**/*.cpp").reject do |f|
-      f.include?("enwik9-preproc") || f.include?("sse.cpp") || f.include?("fxcmv1.cpp")
-    end
+    # 1. Gather source files
+    sources = Dir.glob("src/**/*.cpp").reject { |f| f.include?("enwik9-preproc") }
+
+    # 2. Libraries
     libs = OS.mac? ? ["-lpthread"] : ["-lpthread", "-lstdc++"]
-    system ENV.cxx, "-std=c++14", "-O3", *sources, "-o", "cmix", *libs
+
+    # 3. Compilation - The final touch
+    # Add -DNO_SSE for ARM/Linux, or when in doubt
+    build_flags = ["-std=c++14", "-O3", "-I."]
+    if Hardware::CPU.intel?
+      # Build for Intel, with all optimizations enabled
+    else
+      # For ARM, we add -DNO_SSE to remove the Intel-specific functions
+      build_flags << "-DNO_SSE"
+    end
+
+    system ENV.cxx, *build_flags, *sources, "-o", "cmix", *libs
+
+    # 4. Man Page
     (buildpath/"cmix.1").write <<~EOS
       .TH CMIX 1 "January 2026" "1.9" "Cmix Manual"
       .SH NAME
       cmix \\- World-record-holding compression algorithm
-      .SH SYNOPSIS
-      .B cmix
-      [\\fI-options\\fR] \\fIinput\\fR \\fIoutput\\fR
       .SH DESCRIPTION
-      Note: Intel-specific optimizations (SSE/FXCM) are disabled on ARM builds.
+      World-record-holding compression algorithm.
       .SH AUTHOR
       Byron Knoll. Formula by Aritya Arjunan.
     EOS
+
+    # 5. Install
     bin.install "cmix"
     pkgshare.install "dictionary"
     man1.install "cmix.1"
