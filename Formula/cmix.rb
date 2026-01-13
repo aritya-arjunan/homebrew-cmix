a8f59a9352a7c0f7fcc6b320553e7f33e2488eef
Aritya Vitendra Arjunan <aritya.wps@gmail.com>
2026-01-13 13:17:25 +1100

    fixed style

--- Formula/cmix.rb
+++ Formula/cmix.rb
@@ -6,20 +6,14 @@ class Cmix < Formula
   license "GPL-3.0-or-later"
 
   def install
-    # Maximize CPU features for M-series and Intel
-    # -O3 is for maximum speed optimization
     libs = OS.mac? ? "-lpthread" : "-lpthread -lstdc++"
-    
-    # system ENV.cxx automatically handles M1, M2, M3, M4, M5, and Intel
     system ENV.cxx, "-O3", "cmix.cpp", "-o", "cmix", *libs.split
-    
     bin.install "cmix"
     pkgshare.install "dictionary"
     man1.install "cmix.1"
   end
 
   test do
-    # Verify binary exists and runs
     output = shell_output("#{bin}/cmix", 1)
     assert_match "cmix", output
   end
