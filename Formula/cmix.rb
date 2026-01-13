ba5273b6dee2ff176fadfb45a0a28f9645791a2c
Aritya Vitendra Arjunan <aritya.wps@gmail.com>
2026-01-13 13:45:06 +1100

    recursive file search

--- Formula/cmix.rb
+++ Formula/cmix.rb
@@ -1,3 +1,5 @@
+require "find"
+
 class Cmix < Formula
   desc "World-record-holding compression algorithm"
   homepage "https://github.com/byronknoll/cmix"
@@ -6,45 +8,55 @@ class Cmix < Formula
   license "GPL-3.0-or-later"
 
   def install
-    # 1. FIND THE CODE:
-    # This finds the directory containing cmix.cpp, no matter what it is named.
-    src_file = Dir.glob("**/cmix.cpp").first
-    odie "Could not find cmix.cpp in the download!" if src_file.nil?
-    src_dir = File.dirname(src_file)
-
-    # 2. GO TO THE CODE AND BUILD:
-    Dir.chdir(src_dir) do
-      # Create the man page inside the source directory so it's not lost
-      File.write("cmix.1", <<~EOS)
-        .TH CMIX 1 "January 2026" "1.9" "Cmix Manual"
-        .SH NAME
-        cmix \\- World-record-holding compression algorithm
-        .SH SYNOPSIS
-        .B cmix
-        [\\fI-options\\fR] \\fIinput\\fR \\fIoutput\\fR
-        .SH DESCRIPTION
-        .B cmix
-        is an ultra-high-pressure compression tool using context mixing.
-        .SH AUTHOR
-        Byron Knoll. Formula by Aritya Arjunan.
-      EOS
-
-      # Determine libraries (Linux needs -lstdc++ explicitly)
-      libs = OS.mac? ? "-lpthread" : "-lpthread -lstdc++"
-
-      # Run the compiler
-      system ENV.cxx, "-O3", "cmix.cpp", "-o", "cmix", *libs.split
-
-      # Install everything from the source directory
-      bin.install "cmix"
-      pkgshare.install "dictionary"
-      man1.install "cmix.1"
+    # 1. LOCATE THE SOURCE FILE
+    # We stop guessing folder names. We scan the extracted files for 'cmix.cpp'.
+    cpp_file = nil
+    Find.find(buildpath) do |path|
+      if File.basename(path) == "cmix.cpp"
+        cpp_file = path
+        break
+      end
     end
+
+    # 2. DEBUGGING OUTPUT
+    # If the file is somehow missing, this 'ls -R' will print the exact folder structure
+    # to the error log so we can see what is happening.
+    if cpp_file.nil?
+      puts "--- DIRECTORY DUMP ---"
+      system "ls", "-R"
+      puts "----------------------"
+      odie "CRITICAL: cmix.cpp not found. Check directory dump above."
+    end
+
+    # 3. COMPILE
+    # We point the compiler at the absolute path we just found.
+    # We include -lstdc++ for Linux compatibility.
+    libs = OS.mac? ? "-lpthread" : "-lpthread -lstdc++"
+    system ENV.cxx, "-O3", cpp_file, "-o", "cmix", *libs.split
+
+    # 4. MAN PAGE & INSTALL
+    # We write the man page directly to the install location to avoid path errors.
+    (buildpath/"cmix.1").write <<~EOS
+      .TH CMIX 1 "January 2026" "1.9" "Cmix Manual"
+      .SH NAME
+      cmix \\- World-record-holding compression algorithm
+      .SH SYNOPSIS
+      .B cmix
+      [\\fI-options\\fR] \\fIinput\\fR \\fIoutput\\fR
+      .SH DESCRIPTION
+      .B cmix
+      is an ultra-high-pressure compression tool using context mixing.
+      .SH AUTHOR
+      Byron Knoll. Formula by Aritya Arjunan.
+    EOS
+
+    bin.install "cmix"
+    pkgshare.install "dictionary"
+    man1.install "cmix.1"
   end
 
   test do
-    # Cmix usually returns help/error when run without args.
-    # We just want to make sure the binary exists and executes.
-    assert_path_exists bin/"cmix"
+    output = shell_output("#{bin}/cmix", 1)
+    assert_match "cmix", output
   end
 end
