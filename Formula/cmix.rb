82c31eb285114aa1025edef4c16dec6bcee6b46e
Aritya Vitendra Arjunan <aritya.wps@gmail.com>
2026-01-13 14:55:49 +1100

    adding linux 20.04

--- Formula/cmix.rb
+++ Formula/cmix.rb
@@ -6,27 +6,23 @@ class Cmix < Formula
   license "GPL-3.0-or-later"
 
   def install
-    # 1. Gather all source files
     sources = Dir.glob("src/**/*.cpp").reject do |f|
       f.include?("enwik9-preproc") || f.include?("sse.cpp") || f.include?("fxcmv1.cpp")
     end
-
-    # 2. Libraries (Linux needs stdc++)
     libs = OS.mac? ? ["-lpthread"] : ["-lpthread", "-lstdc++"]
-
-    # 3. Compile - We rely on the standard compiler provided by the runner
     system ENV.cxx, "-std=c++14", "-O3", *sources, "-o", "cmix", *libs
-
-    # 4. Man Page
     (buildpath/"cmix.1").write <<~EOS
       .TH CMIX 1 "January 2026" "1.9" "Cmix Manual"
       .SH NAME
       cmix \\- World-record-holding compression algorithm
+      .SH SYNOPSIS
+      .B cmix
+      [\\fI-options\\fR] \\fIinput\\fR \\fIoutput\\fR
+      .SH DESCRIPTION
+      Note: Intel-specific optimizations (SSE/FXCM) are disabled on ARM builds.
       .SH AUTHOR
       Byron Knoll. Formula by Aritya Arjunan.
     EOS
-
-    # 5. Install
     bin.install "cmix"
     pkgshare.install "dictionary"
     man1.install "cmix.1"
