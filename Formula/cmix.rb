4653d7fc8d412c46a470fc5c80f81957b927937a
Aritya Vitendra Arjunan <aritya.wps@gmail.com>
2026-01-13 14:34:25 +1100

    fix arm

--- Formula/cmix.rb
+++ Formula/cmix.rb
@@ -6,11 +6,28 @@ class Cmix < Formula
   license "GPL-3.0-or-later"
 
   def install
-    sources = Dir.glob("src/**/*.cpp").reject do |f|
-      f.include?("enwik9-preproc")
+    # 1. Gather all source files
+    sources = Dir.glob("src/**/*.cpp")
+
+    # 2. RUTHLESS EXCLUSION
+    # We remove the enwik9 tool (conflicting main)
+    # AND on ARM (Mac M-series / Linux ARM), we remove the Intel-only files
+    sources.reject! do |f|
+      f.include?("enwik9-preproc") || 
+      (Hardware::CPU.arm? && (f.include?("sse.cpp") || f.include?("fxcmv1.cpp")))
     end
+
+    # 3. Libraries
     libs = OS.mac? ? ["-lpthread"] : ["-lpthread", "-lstdc++"]
-    system ENV.cxx, "-std=c++14", "-O3", *sources, "-o", "cmix", *libs
+
+    # 4. Compile
+    # We add -D flags to tell the code to stay in 'portable' mode on ARM
+    build_flags = ["-std=c++14", "-O3"]
+    build_flags << "-DNO_SSE" if Hardware::CPU.arm?
+
+    system ENV.cxx, *build_flags, *sources, "-o", "cmix", *libs
+
+    # 5. Create the Man Page
     (buildpath/"cmix.1").write <<~EOS
       .TH CMIX 1 "January 2026" "1.9" "Cmix Manual"
       .SH NAME
@@ -24,12 +41,15 @@ class Cmix < Formula
       .SH AUTHOR
       Byron Knoll. Formula by Aritya Arjunan.
     EOS
+
+    # 6. Install
     bin.install "cmix"
     pkgshare.install "dictionary"
     man1.install "cmix.1"
   end
 
   test do
+    # Run version check; ignore exit code 255
     output = shell_output("#{bin}/cmix", 255)
     assert_match "cmix version", output
   end
